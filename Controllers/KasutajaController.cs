9dfc88a67baba173c862596e631024a329fa41c0
opilane <opilane@HP-CZC2349HTB>
2024-10-24 11:25:44 +0300

    1.5

---
+++ Controllers/KasutajaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace veebRogovski.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KasutajaController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public KasutajaController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
