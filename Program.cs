6b7c1c55d03a580b10ae5dc3146a50d9295d8e74
opilane <opilane@HP-CZC2349HTB>
2024-10-29 14:31:05 +0200

    1.3

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddHttpClient();
+//builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
